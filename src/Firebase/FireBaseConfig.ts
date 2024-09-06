9c115d92fcaf68954941dceba008b2dd15cdcf7a
ASHWIN <meashwinclarence@gmail.com>
2024-09-06 19:03:36 +0530

    fix: firebase config updated

--- src/Firebase/FireBaseConfig.ts
+++ src/Firebase/FireBaseConfig.ts
@@ -4,16 +4,16 @@ import { collection, getFirestore } from "firebase/firestore";
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCSXxXKEC_RQDbkyg5hcfdaylR_nlqpAqk",
-    authDomain: "netflix-clone-6f89d.firebaseapp.com",
-    projectId: "netflix-clone-6f89d",
-    storageBucket: "netflix-clone-6f89d.appspot.com",
-    messagingSenderId: "401069590641",
-    appId: "1:401069590641:web:54fd408095daa2107cb3d0",
-    measurementId: "G-V85HG4470J"
-};
-  
+    apiKey: "AIzaSyBmCHeh9PNmT1TxS8H2phhydfSqN8-gqVM",
+    authDomain: "netflix-clone-6f71f.firebaseapp.com",
+    projectId: "netflix-clone-6f71f",
+    storageBucket: "netflix-clone-6f71f.appspot.com",
+    messagingSenderId: "287651197447",
+    appId: "1:287651197447:web:69ab09eb5dfd173f6b70a2",
+    measurementId: "G-2PTEHQH3E2"
+  };
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app);
